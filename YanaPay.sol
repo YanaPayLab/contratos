f41b0b48df85efd972c7e46f0548bb29e9190923
Elvis Edson Basilio Chambi <elvis.2e3@gmail.com>
2023-10-01 11:29:35 -0400

    Update YanaPay.sol

--- YanaPay.sol
+++ YanaPay.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YanaPay is ERC20 {
+    constructor(uint256 initialSupply) ERC20("YanaPay", "YP") {
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
+    }
+}
